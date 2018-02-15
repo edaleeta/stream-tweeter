252d418d522c41a0ad24a66ccf7e0748533b78d2
Edelita Valdez <edaleeta@gmail.com>
2018-02-15 13:42:45 -0800

    Adds differentiations for jobs.

--- apscheduler_handlers.py
+++ apscheduler_handlers.py
@@ -7,8 +7,10 @@ def start_fetching_twitch_data(user_id):
     """Begin fetching data about a user's stream."""
 
     print("Fetching data for user: {}".format(user_id))
+    job_type = "fetch_data"
+    job_id = job_type + str(user_id)
     scheduler.add_job(func=jobs.fetch_twitch_data,
-                      id=str(user_id),
+                      id=job_id,
                       trigger="interval",
                       args=[user_id],
                       replace_existing=True,
@@ -16,5 +18,12 @@ def start_fetching_twitch_data(user_id):
 
 
 def stop_fetching_twitch_data(user_id):
-    """End the currently running job for the user."""
-    scheduler.delete_job(str(user_id))
+    """End the currently running fetch_data job for the user."""
+    user_id = str(user_id)
+    job_type = "fetch_data"
+    stop_job(job_type, user_id)
+
+
+def stop_job(job_type, user_id):
+    """Given a job type and user_id, stop the job."""
+    scheduler.delete_job(job_type + user_id)
