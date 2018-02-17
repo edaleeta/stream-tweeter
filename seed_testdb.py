5cc4d6265cca12b872938f145c5d5de804ac5b18
Edelita Valdez <edaleeta@gmail.com>
2018-02-17 13:46:55 -0800

    Updates seed script for testing to setup and teardown.

--- seed_testdb.py
+++ seed_testdb.py
@@ -1,17 +1,17 @@
 """Seed testdb for Yet Another Twitch Toolkit for interactive use."""
 import atexit
 import server
-from model import *
+import model as m
 
-connect_to_db(server.app, "postgresql:///testdb")
+m.connect_to_db(server.app, "postgresql:///testdb")
 
-db.create_all()
-db.session.commit()
-sample_data()
-import pdb; pdb.set_trace()
+m.db.create_all()
+m.db.session.commit()
+m.sample_data()
+
 
 @atexit.register
 def teardown():
     """Drop all tables in db."""
-    db.session.close()
-    db.drop_all()
+    m.db.session.close()
+    m.db.drop_all()
