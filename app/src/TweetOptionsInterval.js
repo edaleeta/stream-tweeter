c5709655a3d247c60f37a3550eac6aa98c4cb8f2
Edelita Valdez <edaleeta@gmail.com>
2018-02-23 13:07:38 -0800

    Renders tweet interval form with user's current interval.

--- app/src/TweetOptionsInterval.js
+++ app/src/TweetOptionsInterval.js
@@ -4,84 +4,85 @@ import { FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-b
 
 export class TweetOptionsInterval extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            // We actually want to show the user's current tweet interval setting here, or default to 30 if not yet set.
-            value: 30
-        };
-        this.getValidationState = this.getValidationState.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      // We actually want to show the user's current tweet interval setting here, or default to 30 if not yet set.
+      value: this.props.tweetInterval
+    };
+    this.getValidationState = this.getValidationState.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+  
+  handleClick(e) {
+    e.preventDefault();
+
+    // Prevent user from submitting invalid number of minutes.
+    if (this.getValidationState() != null) {
+      return null
     }
 
-    handleChange(e) {
-        this.setState({
-            value: e.target.value
-        });
-    }
-    
-    handleClick(e) {
-        e.preventDefault();
-
-        // Prevent user from submitting invalid number of minutes.
-        if (this.getValidationState() != null) {
-            return null
-        }
-
-        let url = "/api/update-user-settings";
-        let payload = JSON.stringify({
-            userId: this.props.userId,
-            tweetInterval: this.state.value
-        });
-
-        console.log(payload);
-
-        fetch(url, {
-            credentials: 'same-origin',
-            method: 'POST',
-            body: payload, 
-            headers: new Headers({
-            'Content-Type': 'application/json'
-            })
-        })
-        .then(res => res.json())
-        .catch(error => console.error('Error:', error))
-        .then(response => {
-            console.log('Success:', response);
+    let url = "/api/update-user-settings";
+    let payload = JSON.stringify({
+      userId: this.props.userId,
+      tweetInterval: this.state.value
     });
 
+    console.log(payload);
+
+    fetch(url, {
+      credentials: 'same-origin',
+      method: 'POST',
+      body: payload, 
+      headers: new Headers({
+      'Content-Type': 'application/json'
+      })
+    })
+    .then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {
+      console.log('Success:', response);
+  });
 
 
-    }
-    getValidationState() {
-        if (this.state.value < 30) {
-            return "warning";
-        }
-        return null;
-    }
 
-    render() {
-        return (
-            <div>
-                <form>
-                    <FormGroup controlId="tweetSettingsForm" validationState={this.getValidationState()} bsSize="lg">
-                    <ControlLabel>Tweet Interval in Minutes</ControlLabel>
-                    <FormControl
-                        type="number"
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                    />
-                    <FormControl.Feedback/>
-                    <HelpBlock style={ this.getValidationState() === null ? {display: "none"} : {display: "block"} }>Don't spam your friends! Value must be greater than 30 minutes.</HelpBlock>
-                    <Button type="submit" onClick={this.handleClick} value={this.state.value} >Tweet Every {this.state.value} minutes!</Button>
-                    </FormGroup>
-                </form>
-            </div>
-        ); 
+  }
+  getValidationState() {
+    if (this.state.value < 30) {
+      return "warning";
     }
+    return null;
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <FormGroup controlId="tweetSettingsForm" validationState={this.getValidationState()} bsSize="lg">
+          <ControlLabel>Tweet Interval in Minutes</ControlLabel>
+          <FormControl
+            type="number"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback/>
+          <HelpBlock style={ this.getValidationState() === null ? {display: "none"} : {display: "block"} }>Don't spam your friends! Value must be greater than 30 minutes.</HelpBlock>
+          <Button type="submit" onClick={this.handleClick} value={this.state.value} >Tweet Every {this.state.value} minutes!</Button>
+          </FormGroup>
+        </form>
+      </div>
+    ); 
+  }
 }
 
 TweetOptionsInterval.propTypes = {
-    userId: PropTypes.number.isRequired
+  userId: PropTypes.number.isRequired,
+  tweetInterval: PropTypes.number.isRequired
 }
