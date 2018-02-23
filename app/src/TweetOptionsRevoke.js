0c62170efb052b0574f0d1766df46c84b97faf41
Edelita Valdez <edaleeta@gmail.com>
2018-02-23 12:29:09 -0800

    Allows user to disconnect Twitter account.

---
+++ app/src/TweetOptionsRevoke.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+
+export class TweetOptionsRevoke extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        console.log(e.target.value);
+
+        let url = "/api/revoke-twitter";
+        let payload = JSON.stringify({"userId": this.props.userId});
+
+        fetch(url, {
+            credentials: 'same-origin',
+            method: 'POST',
+            body: payload, 
+            headers: new Headers({
+            'Content-Type': 'application/json'
+            })
+        })
+        .then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => {
+            console.log('Success:', response);
+            if ("success" in response) {
+                // Tells App that Twitter is no longer authenticated
+                this.props.onClick();
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Button bsStyle="danger"
+                value={this.props.userId}
+                onClick={this.handleClick}>
+                Disconnect Twitter Account
+            </Button>
+        )
+    }
+}
+
+TweetOptionsRevoke.propTypes = {
+    userId: PropTypes.number.isRequired,
+    onClick: PropTypes.func.isRequired
+}
