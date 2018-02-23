33aae93e48c81723f28a01fea5384e6dff9bcb23
Edelita Valdez <edaleeta@gmail.com>
2018-02-23 13:03:49 -0800

    TwitterOptions hands down tweet interval.

--- app/src/TwitterOptions.js
+++ app/src/TwitterOptions.js
@@ -4,31 +4,38 @@ import { TweetOptionsInterval } from './TweetOptionsInterval'
 import { TweetOptionsRevoke } from './TweetOptionsRevoke'
 
 export class TweetOptions extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            isUpdated: false
-        }
-    };
-
-    render() {
-        if (this.props.isTwitterAuth) {
-            return (
-                <div>
-                    <h2>Twitter Options</h2>
-                    <TweetOptionsInterval userId={this.props.userId} />
-                    <TweetOptionsRevoke userId={this.props.userId} onClick={this.props.onClick} />
-                </div>
-            ); 
-        } else {
-            return <div></div>
-        }
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUpdated: false
     }
+  };
+
+  render() {
+    if (this.props.isTwitterAuth) {
+      return (
+        <div>
+          <h2>Twitter Options</h2>
+          <TweetOptionsInterval
+            userId={this.props.userId}
+            tweetInterval={this.props.tweetInterval}
+          />
+          <TweetOptionsRevoke
+            userId={this.props.userId}
+            tweetInterval={this.props.tweetInterval}
+            onClick={this.props.onClick} />
+        </div>
+      ); 
+    } else {
+      return <div></div>
+    }
+  }
 }
 
 TweetOptions.propTypes = {
-    isTwitterAuth: PropTypes.bool.isRequired,
-    userId: PropTypes.number.isRequired,
-    onClick: PropTypes.func.isRequired
+  isTwitterAuth: PropTypes.bool.isRequired,
+  userId: PropTypes.number.isRequired,
+  tweetInterval: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
 }
