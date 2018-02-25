8b5c5412a26df6401eb410a8b0a025183705fc5e
Edelita Valdez <edaleeta@gmail.com>
2018-02-24 18:59:52 -0800

    Displays fetched stream session info.

--- app/src/StreamSessions.js
+++ app/src/StreamSessions.js
@@ -6,23 +6,36 @@ export class StreamSessions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isUpdated: false
+      streams: "",
+      nextPage: ""
     };
-    this.onClickUpdateStreamSessionsCurrent = this.onClickUpdateStreamSessionsCurrent.bind(this);
   }
 
-  onClickUpdateStreamSessionsCurrent() {
-    this.setState({
-      isUpdated: true
-    });
+  componentWillMount() {
+    fetch("/api/stream-sessions",
+    {credentials: 'same-origin'})
+    .then((response)=> response.json())
+    .then((data) => {
+      console.log("StreamSessions mounted!");
+      console.log(data.streams);
+      console.log(data.next);
+      this.setState({
+        streams: data.streams,
+        nextPage: data.next
+      });
+    })        
   }
 
   render() {
-    return (
-      <div>
-        I'm going to display stream sessions.
-      </div>
-    ); 
+    if (this.state.streams) {
+      return (
+        this.state.streams.map((stream, key) => (
+          <div key={key}>{stream.startedAt} {stream.endedAt} {stream.twitchSessionId} {stream.userId}</div>
+        ))
+      ); 
+    } else {
+      return <div></div>
+    }
   }
 }
 
