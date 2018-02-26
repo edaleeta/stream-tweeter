c61cd7daf5c130b12aca482ca0e5a1d8048abf57
Edelita Valdez <edaleeta@gmail.com>
2018-02-25 21:35:20 -0800

    Adds test clip to embed; wraps app in bootstrap grid.

--- app/src/index.js
+++ app/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Grid } from 'react-bootstrap';
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+    <Grid>
+      <App />
+    </Grid>
   </BrowserRouter>
 ), document.getElementById('root'));
 registerServiceWorker();
