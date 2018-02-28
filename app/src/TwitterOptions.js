700a10b0551803f625977dca0e7cb243018b8992
Edelita Valdez <edaleeta@gmail.com>
2018-02-28 15:06:42 -0800

    Adds row and col structure to tweet options.

--- app/src/TwitterOptions.js
+++ app/src/TwitterOptions.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { TweetOptionsInterval } from './TweetOptionsInterval'
 import { TweetOptionsRevoke } from './TweetOptionsRevoke'
 import { StartTweetingButton } from './StartTweetingButton'
+import { Row, Col } from 'react-bootstrap';
 
 export class TweetOptions extends Component {
   
@@ -17,17 +18,25 @@ export class TweetOptions extends Component {
     if (this.props.isTwitterAuth) {
       return (
         <div>
-          <h2>Tweet Options</h2>
-          <TweetOptionsInterval
-            userId={this.props.userId}
-            tweetInterval={this.props.tweetInterval}
-          />
-          <TweetOptionsRevoke
-            userId={this.props.userId}
-            tweetInterval={this.props.tweetInterval}
-            onClick={this.props.onClick}
-          />
-          <StartTweetingButton userId={this.props.userId} />
+          <Row>
+            <h2>Tweet Options</h2>
+          </Row>
+          <Row>
+            <TweetOptionsInterval
+              userId={this.props.userId}
+              tweetInterval={this.props.tweetInterval}
+            />
+          </Row>
+          <Row>
+            <StartTweetingButton userId={this.props.userId} />
+          </Row>
+          <Row>
+            <TweetOptionsRevoke
+              userId={this.props.userId}
+              tweetInterval={this.props.tweetInterval}
+              onClick={this.props.onClick}
+            />
+          </Row>
         </div>
       ); 
     } else {
