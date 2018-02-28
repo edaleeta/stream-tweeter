8b1e7b969459845363859bd21c53878bfe931dca
Edelita Valdez <edaleeta@gmail.com>
2018-02-28 14:48:50 -0800

    Adds row and column structure to landing page.

--- app/src/Home.js
+++ app/src/Home.js
@@ -3,23 +3,32 @@ import PropTypes from 'prop-types';
 import { ConnectTwitter } from './ConnectTwitter'
 import { TweetTemplates } from './TweetTemplates'
 import { TweetOptions } from './TwitterOptions'
+import { Row, Col } from 'react-bootstrap';
 
 export class Home extends Component {
 
   render() {
     return (
-      <div>
-        <ConnectTwitter isTwitterAuth={this.props.isTwitterAuth} />
-        <TweetOptions
-          isTwitterAuth={this.props.isTwitterAuth}
-          userId={this.props.userId}
-          tweetInterval={this.props.tweetInterval}
-          onClick={this.props.onClick}
-        />
-        <TweetTemplates isTwitterAuth={this.props.isTwitterAuth}
-          userId={this.props.userId}
-        />
-      </div>
+      <Row>
+          <Row>
+            <ConnectTwitter isTwitterAuth={this.props.isTwitterAuth} />
+          </Row>
+          <Row>
+            <Col xs={12} md={8}>
+              <TweetTemplates isTwitterAuth={this.props.isTwitterAuth}
+                userId={this.props.userId}
+              />
+            </Col>
+            <Col xs={12} md={4}>
+              <TweetOptions
+                isTwitterAuth={this.props.isTwitterAuth}
+                userId={this.props.userId}
+                tweetInterval={this.props.tweetInterval}
+                onClick={this.props.onClick}
+              />
+            </Col>
+          </Row>
+      </Row>
     )
   
   }
