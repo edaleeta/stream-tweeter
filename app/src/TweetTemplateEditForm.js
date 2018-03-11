572f31ba07e0d49dcca05cb7670d3d029c3bc60f
Edelita Valdez <edaleeta@gmail.com>
2018-03-11 16:39:29 -0700

    Adds better UX to updating tweet templates; prevents blank tweets.

--- app/src/TweetTemplateEditForm.js
+++ app/src/TweetTemplateEditForm.js
@@ -5,7 +5,8 @@ import { FormGroup,
          FormControl,
          Button,
          ButtonToolbar,
-        Collapse} from 'react-bootstrap';
+         Collapse,
+         HelpBlock } from 'react-bootstrap';
 
 export class TweetTemplateEditForm extends Component {
 
@@ -13,7 +14,8 @@ export class TweetTemplateEditForm extends Component {
     super(props);
     this.state = {
       contents: this.props.contents,
-      originalContents: this.props.contents
+      originalContents: this.props.contents,
+      validationState: null
     };
     this.handleSaveClick = this.handleSaveClick.bind(this);
     this.handleResetClick = this.handleResetClick.bind(this);
@@ -22,8 +24,17 @@ export class TweetTemplateEditForm extends Component {
 
   handleSaveClick(e) {
     e.preventDefault();
-    console.log(this.props.templateId);
     console.log(this.state.contents.trim());
+    if (this.state.contents.trim() == "") {
+      this.setState({
+        validationState: "warning"
+      });
+      return;
+    } else {
+      this.setState({
+        validationState: null
+      })
+    }
 
     let url = "/api/edit-tweet-template"
     let payload = JSON.stringify({
@@ -47,9 +58,9 @@ export class TweetTemplateEditForm extends Component {
       console.log('Success:', response);
       // Tells Tweet Templates that we saved a new Template!
       this.props.onClick();
-      // Clear contents of textbox after submitting.
+      // On successful update, update originalContents with new contents.
       this.setState({
-        contents: this.state.originalContents
+        originalContents: this.state.contents.trim()
       });
     })
   }
@@ -67,12 +78,20 @@ export class TweetTemplateEditForm extends Component {
   }
 
   render() {
+    let helpText;
+    if (this.state.validationState == "warning") {
+      helpText = (
+        <HelpBlock>Your Tweet must contain text!</HelpBlock>
+      )
+    } else {
+      helpText = <span></span>
+    }
 
     return (
       <Collapse in={!this.props.hidden}>
         <div>
           <form>
-            <FormGroup>
+            <FormGroup validationState={this.state.validationState}>
               <ControlLabel><h4>Update Tweet Template</h4></ControlLabel>
               <FormControl
                 onChange={this.handleChange}
@@ -80,6 +99,7 @@ export class TweetTemplateEditForm extends Component {
                 value={this.state.contents}
                 rows={3}>
               </FormControl>
+              {helpText}
               <ButtonToolbar style={{marginTop: "0.5rem"}}>
                 <Button type="submit" onClick={this.handleSaveClick}>Save Changes</Button>
                 <Button onClick={this.handleResetClick}>Reset Changes</Button>
--- app/src/WelcomeUser.js
+++ app/src/WelcomeUser.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import './WelcomeUser.css';
 
 export class WelcomeUser extends Component {
 
